7103db54bfe766a60171c4ccd164a7103393bfbb
mygu <mingyugu0410@gmail.com>
2016-06-20 15:25:05 +0800

    update index.js

--- index.js
+++ index.js
@@ -2,9 +2,35 @@
  * Created by mygu on 16/6/19.
  */
 
-var ReactNative = {
-    // Components
-    Danmaku: require('./Danmaku')
+import React, {
+    requireNativeComponent,
+    PropTypes,
+    View,
+    Component
+} from 'react-native';
+
+class Danmaku extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        const nativeProps = Object.assign({}, this.props);
+        Object.assign(nativeProps, {});
+        return (
+            <RCTDanmaku
+                {...nativeProps}
+            />
+        )
+    }
+}
+
+Danmaku.propTypes = {
+    started: PropTypes.bool,
+    ...View.propTypes
 };
 
-module.exports = ReactNative;
+const RCTDanmaku = requireNativeComponent('RCTDanmaku', Danmaku);
+
+module.exports = Danmaku;
